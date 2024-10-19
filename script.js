d9ad570a7f9495a42bd402319b5ff04c852516cf
Mohammad Atef <161877547+mohammadatefali@users.noreply.github.com>
2024-10-20 01:08:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ authForm.addEventListener("submit", function (event) {
   }
 
   // Email validation regex
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  //const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const emailOrPhoneRegex = /(\w{2,}@(gmail|yahoo).[a-z]{2,}|^01\d{9}$)/gi;
   if (isLogin) {
     if (users[email]) {
       if (users[email].password === password) {
@@ -55,6 +55,19 @@ authForm.addEventListener("submit", function (event) {
       errorMessage.className = "message error";
     }
   } else {
+    if (!emailOrPhoneRegex.test(email)) {
+      if (isNaN(email)) {
+        errorMessage.textContent =
+          " please enter a vaild email address \n for example:example@gmail.com.";
+        errorMessage.className = "message error";
+        return;
+      } else {
+        errorMessage.textContent =
+          " please enter a vaild phone number \n for example:01234569842.";
+        errorMessage.className = "message error";
+        return;
+      }
+    }
     if (users[email]) {
       errorMessage.textContent = "You are already registered. Please login.";
       errorMessage.className = "message error";
